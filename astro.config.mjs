65b8d9248d03a7d3204d7ce70789b9d51fdfc4eb
Cake <cakemancerr@gmail.com>
2023-01-08 00:14:29 +0100

    feat: add compress integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,16 @@
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
+// Integrations
 import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 import image from "@astrojs/image";
+import compress from "astro-compress";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind(),
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
     }),
+    compress(),
   ],
 });
